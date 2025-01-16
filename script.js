ad3e2a6eb281932afeb37d0f38e5984e2b607fd9
Jhonata Godoy <88396979+jhonatagd@users.noreply.github.com>
2025-01-16 18:47:09 -0300

    create var, for created

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("TESTE")
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor")
+let password = document.querySelector("#password")
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvABCDEFGHIJKTMLOP123456789!@#$";
+let novaSenha ="";
+
+sizePassword.innerHTML = sliderElement.value;
+
+sliderElement.oniput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+    
+    let pass = "";
+    for()
+}
